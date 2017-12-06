6e50858fd599d0cbbf9dab601475954ea2ef4476
hamme503 <hamme503@morris.umn.edu>
2017-12-05 21:52:26 -0600

    implement solution for EchoServer

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -9,21 +9,46 @@ import java.net.Socket;
 public class EchoServer {
 	public static final int PORT_NUMBER = 6013;
 
+	
+
 	public static void main(String[] args) throws IOException, InterruptedException {
 		EchoServer server = new EchoServer();
 		server.start();
 	}
 
+	private static class clientThread extends Thread {
+		
+		Socket clientSocket;
+
+		public clientThread(Socket clientSocket) {
+			this.clientSocket = clientSocket;
+		}
+		
+
+		public void run() {
+			try {
+				InputStream inputStream = clientSocket.getInputStream();
+				OutputStream outputStream = clientSocket.getOutputStream();
+				int b;
+				while ((b = inputStream.read()) != -1) {
+					outputStream.write(b);
+				}
+	
+			clientSocket.close();
+			} catch (IOException e) {
+				System.out.println("IOException when running the thread");
+			}
+		}
+	}
+
+
 	private void start() throws IOException, InterruptedException {
 		ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
 		while (true) {
 			Socket socket = serverSocket.accept();
-			InputStream inputStream = socket.getInputStream();
-			OutputStream outputStream = socket.getOutputStream();
-			int b;
-			while ((b = inputStream.read()) != -1) {
-				outputStream.write(b);
-			}
+			clientThread thread = new clientThread(socket);
+			thread.run();
 		}
+
 	}
 }
