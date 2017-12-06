5597f2a8beda3b09eb2ea97c94212ba5164f8f4d
hamme503 <hamme503@morris.umn.edu>
2017-12-05 21:51:59 -0600

    implement solution for EchoClient

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -8,6 +8,61 @@ import java.net.Socket;
 public class EchoClient {
 	public static final int PORT_NUMBER = 6013;
 
+	private static class inputThread {
+
+		Socket socket;
+
+		public inputThread(Socket socket) {
+			this.socket = socket;
+		}
+
+		public void run() {
+			try {
+				InputStream systemIn = System.in;
+				OutputStream socketOutputStream = socket.getOutputStream();
+
+				int readByte;
+				while ((readByte = systemIn.read()) != -1) {
+					socketOutputStream.write(readByte);
+				}
+
+				socket.shutdownOutput();
+			} catch (IOException e) {
+				System.out.println("Error while gathering user input");
+			}
+		}
+
+	}
+
+	private static class outputThread {
+
+		Socket socket;
+
+		public outputThread(Socket socket) {
+			this.socket = socket;
+		}
+
+		public void run() {
+
+			try {
+				OutputStream systemOut = System.out;
+				InputStream socketInputStream = socket.getInputStream();
+
+				int readByte;
+				while ((readByte = socketInputStream.read()) != -1) {
+					systemOut.write(readByte);
+				}
+
+				System.out.flush();
+
+			} catch (IOException e) {
+				System.out.println("Error when processing server output");
+			}
+
+		}
+
+	}
+
 	public static void main(String[] args) throws IOException {
 		EchoClient client = new EchoClient();
 		client.start();
@@ -15,14 +70,10 @@ public class EchoClient {
 
 	private void start() throws IOException {
 		Socket socket = new Socket("localhost", PORT_NUMBER);
-		InputStream socketInputStream = socket.getInputStream();
-		OutputStream socketOutputStream = socket.getOutputStream();
-		int readByte;
-		while ((readByte = System.in.read()) != -1) {
-			socketOutputStream.write(readByte);
-			int socketByte = socketInputStream.read();
-			System.out.write(socketByte);
-		}
-		System.out.flush();
+		outputThread outThread = new outputThread(socket);
+		inputThread inThread = new inputThread(socket);
+
+		inThread.run();
+		outThread.run();
 	}
 }
